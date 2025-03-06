e75cb72f44d66d25e8e0b3371c5b6f64650491c0
SATHWIK <sathwikacharya61@gmail.com>
2025-03-06 11:48:12 +0530

    D

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,22 +1,30 @@
-import { useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)  
-  useEffect(async()=>{
-    const response = await fetch('https://catfact.ninja/fact')
-    .then(response => response.json())
-    .then(data => console.log(data));
-            
-  },[count]);
+  const [count, setCount] = useState({ fact: "", length: 0 });
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("https://catfact.ninja/fact");
+        const data = await response.json();
+        setCount(data);
+        console.log("Fetched Data:", data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <>
-      <h1>{count}</h1>
-      <button type="button" onClick={()=>{setCount(count+1 )}}>Update</button>
+      <p>{count.fact}</p>
+      <p>Length: {count.length}</p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
